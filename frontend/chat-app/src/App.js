bfa8c048019c4dc9194489baac1dcadba4885e63
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-01-15 17:09:25 +0530

    routing

--- frontend/chat-app/src/App.js
+++ frontend/chat-app/src/App.js
@@ -1,22 +1,33 @@
 import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Layout from './pages/Layout';
 
 function App() {
-   const [data, setData] = useState('');
+  const baseUrl = 'http://localhost:5000'
+  const [data, setData] = useState('');
 
-   useEffect(() => {
-      fetch('http://localhost:5000', {
-         method: 'GET',
-      })
-         .then((response) => response.text()) // Assuming the response is plain text
-         .then((data) => setData(data))
-         .catch((err) => console.error('Error fetching data:', err));
-   }, []);
+  useEffect(() => {
+    fetch(`${baseUrl}/`, {
+      method: 'GET',
+    })
+      .then((response) => response.text()) // Assuming the response is plain text
+      .then((data) => setData(data))
+      .catch((err) => console.error('Error fetching data:', err));
+  }, []);
 
-   return (
-      <div>
-         <p>{data}</p>
-      </div>
-   );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          {/* <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
